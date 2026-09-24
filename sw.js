0d5bba6ae671976141727f0e55e475a3abed0d19
piotrrgw <246545467+piotrrgw@users.noreply.github.com>
2026-09-24 21:12:39 +0200

    1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wykaz-v3.4-cache';
+const CACHE_NAME = 'wykaz-v3.8-cache';
 const ASSETS = [
   './',
   './index.html',
@@ -8,8 +8,22 @@ const ASSETS = [
 
 // Instalacja i cache'owanie zasobów
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Zmusza apkę do natychmiastowej instalacji nowej wersji!
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Czyszczenie starych wersji po aktywacji
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    }).then(() => self.clients.claim())
   );
 });
 
